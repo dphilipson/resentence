880af5b8aec4342e3ded4f001747a6b6d2cd79cc
David Philipson <david.philipson@gmail.com>
2019-01-02 19:51:37 -0800

    Updates

---
+++ precache-manifest.3839ccca3e1f2aec5666329b3a21168b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1029e56e7db5fa70d5f7",
+    "url": "/resentence/static/css/main.52b4591e.chunk.css"
+  },
+  {
+    "revision": "1029e56e7db5fa70d5f7",
+    "url": "/resentence/static/js/main.1029e56e.chunk.js"
+  },
+  {
+    "revision": "467b3058b6256204369d",
+    "url": "/resentence/static/css/1.1ba7762a.chunk.css"
+  },
+  {
+    "revision": "467b3058b6256204369d",
+    "url": "/resentence/static/js/1.467b3058.chunk.js"
+  },
+  {
+    "revision": "b7fc1e62de95ffb5fb12",
+    "url": "/resentence/static/js/runtime~main.b7fc1e62.js"
+  },
+  {
+    "revision": "d0bcd5b9effbe2570868f8c906ea7792",
+    "url": "/resentence/static/media/icons-16.d0bcd5b9.woff"
+  },
+  {
+    "revision": "26e98f292f486f2bfac2abf9472e490a",
+    "url": "/resentence/static/media/icons-16.26e98f29.ttf"
+  },
+  {
+    "revision": "5dbae370414339a081c1c6fac80659cc",
+    "url": "/resentence/static/media/icons-16.5dbae370.eot"
+  },
+  {
+    "revision": "9d0578e5315c18aa528db1f1bfcb56f5",
+    "url": "/resentence/static/media/icons-20.9d0578e5.woff"
+  },
+  {
+    "revision": "0ec8ac2bacb2c3a1b2a2064305bc1223",
+    "url": "/resentence/static/media/icons-20.0ec8ac2b.ttf"
+  },
+  {
+    "revision": "f4d910a0626d8e60f310a7199dc6b191",
+    "url": "/resentence/static/media/icons-20.f4d910a0.eot"
+  },
+  {
+    "revision": "47f977f9876bc2d4845efee56015ab57",
+    "url": "/resentence/index.html"
+  }
+];
